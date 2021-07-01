e2df048aa1bd5260070d7ac77b6632baf9c39aa7
jcesarmobile <jcesarmobile@gmail.com>
2021-07-01 19:47:30 +0200

    initial commit

--- appboundsbug/ViewController.swift
+++ appboundsbug/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
     }
-
-
+    
+    var webView: WKWebView!
+    var contentController = WKUserContentController()
+    override func viewDidLoad() {
+    super.viewDidLoad()
+        let request = URLRequest(url: URL(string: "app://localhost/index.html")!)
+        _ = webView?.load(request)
+    }
+    override func loadView() {
+        let webViewConfiguration = WKWebViewConfiguration()
+        webViewConfiguration.setURLSchemeHandler(WebViewHander(), forURLScheme: "app")
+        let data = "document.body.style.background = 'red';"
+        let userScript = WKUserScript(source: data, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+        contentController.addUserScript(userScript)
+        webViewConfiguration.userContentController = contentController
+        webView = WKWebView(frame: .zero, configuration: webViewConfiguration)
+        webView.navigationDelegate = self
+        view = webView
+    }
 }
 
